4e92f6c7e62823a46f919e6c7985f39e9f56a648
Xrtero <Xrtero@gmail.com>
2022-01-08 17:37:48 +0800

    add bubble sort

---
+++ src/main/java/com/xrtero/sort/BubbleSort.java
@@ -0,0 +1,46 @@
+package com.xrtero.sort;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Xrtero
+ */
+public class BubbleSort {
+
+    /**
+     * 冒泡排序
+     *
+     * @param arr
+     */
+    public static void sort(int[] arr){
+       int n = arr.length;
+       boolean isSwap = true;
+       while(isSwap){
+           isSwap = false;
+           for(int i=0;i<n-1;i++){
+               if(arr[i]>arr[i+1]){
+                   isSwap = true;
+                   // 交换两个数，为了确保溢出，使用临时变量存储
+                   int temp = arr[i];
+                   arr[i] = arr[i+1];
+                   arr[i+1] = temp;
+               }
+           }
+       }
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{4,2,1,3};
+        sort(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.printf(" " + arr[i]);
+        }
+        System.out.println("\n");
+
+    }
+
+
+
+}
